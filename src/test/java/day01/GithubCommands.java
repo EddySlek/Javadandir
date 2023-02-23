fdf94bbf579b558dca5c5caeb6d5d54c21513376
EddySlek <slek.eddy@gmail.com>
2023-02-23 09:54:48 -0700

    second time commit

---
+++ src/test/java/day01/GithubCommands.java
@@ -0,0 +1,31 @@
+package day01;
+
+public class GithubCommands {
+    /*
+    GITHUB - Command:
+
+    1) Sadece bir defa kullanilir. Eger daha once github ve git kurmadiysan.
+    git init
+    git config --global user.name "EddySlek"
+    git config --global user.email "slek.eddy@gmail.com"
+    git config credential.username "EddySlek"
+    git config credential.password "xaxsdwwq"
+    git config --list
+
+    2) Eger github credential yapildiysa pc de ve ilk defa projeye baslanilacaksa:
+    git init (Yeni Proje baslanildiginda sadece bir defaf yazilir)
+    git status
+    git add .
+    git commit -m "first commit"
+    git remote add origin https://github.com/EddySlek/Javadandir.git
+    git push --set-upstream origin master
+    git push
+    git branch
+
+
+     */
+
+
+
+
+}
