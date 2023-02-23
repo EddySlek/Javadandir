45f6358d049e43175ba25374c2beda5a1782f6ac
EddySlek <slek.eddy@gmail.com>
2023-02-22 18:54:35 -0700

    first commit

---
+++ src/test/java/day01/Fields_Methods_DataType.java
@@ -0,0 +1,28 @@
+package day01;
+
+public class Fields_Methods_DataType {
+    /*
+    GITHUB - Command:
+
+    git status
+    git init
+
+    1) =>Sadece bir defa kullanilir. Eger daha once github ve git kurmadiysan.
+    git config --global user.name "EddySlek"
+    git config --global user.email "slek.eddy@gmail.com"
+    git config credential.username "EddySlek"
+    git config credential.password "xaxsdwwq"
+    git config --list
+
+    2) => Eger github credential yapildiysa pc de:
+    git init
+    git status
+    git add .
+
+
+     */
+
+
+
+
+}
