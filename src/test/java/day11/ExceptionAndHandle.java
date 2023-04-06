7ca6113e3e6fe2d4471f32b259fa0828a802c17c
EddySlek <slek.eddy@gmail.com>
2023-04-06 04:57:08 -0600

    Ed

---
+++ src/test/java/day11/ExceptionAndHandle.java
@@ -0,0 +1,18 @@
+package day11;
+
+import java.io.IOException;
+
+public class ExceptionAndHandle {
+
+    static int name;
+
+    public static void main(String[] args) {
+
+        System.out.println(name);
+
+
+    }
+
+
+
+}
