90d95f8de9666091e6f98888f4999fe13f7af712
EddySlek <slek.eddy@gmail.com>
2023-04-19 19:51:49 -0600

    Ed

--- src/test/java/day11/Pr2.java
+++ src/test/java/day11/Pr2.java
@@ -9,11 +9,20 @@ public class Pr2 {
         Scanner scan = new Scanner(System.in);
         System.out.println("enter a positive number");
         int nbr = scan.nextInt();//4578
+
         String number = Integer.toString(nbr);
+
+        // Convert
+        double d =12.2;
+        String nn = Double.toString(d);
+        long l = Long.parseLong(nn);
+
+        // int to String
 
         int resul=0;
         for(int i=0; i<number.length();i++){
             int n = Integer.parseInt(String.valueOf(number.charAt(i)));
+            // String to int
             int s = (int) Math.pow(n,3);
             resul = resul+s;
 
