4b5abbc094c3bf851dfbaacfa2acfb42b0527f72
EddySlek <slek.eddy@gmail.com>
2023-06-20 17:10:04 -0600

    it is a new class

---
+++ src/test/java/Try.java
@@ -0,0 +1,11 @@
+public class Try {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Earth, which mean we are not living there!");
+        System.err.println("Hello Earth, which mean we are not living there!");
+
+    }
+
+
+}
