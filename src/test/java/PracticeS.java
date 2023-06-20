1ca48fa7f325d10c9c6a2a17829222b6a7a84d2b
EddySlek <slek.eddy@gmail.com>
2023-06-20 11:19:38 -0600

    Ed

--- src/test/java/PracticeS.java
+++ src/test/java/PracticeS.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PracticeS {
 
+    public static void main(String[] arg) {
+        List<String> nn = new ArrayList<>();
+
+        nn.add("new");
+        System.out.println(nn);
+
+    }
 
 }
 
