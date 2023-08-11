0ba7bff402a1d901755ee684c25fb1b96ca5d725
EddySlek <slek.eddy@gmail.com>
2023-08-10 21:47:28 -0600

    it is a new class

--- src/test/java/den/FirstClass.java
+++ src/test/java/den/FirstClass.java
@@ -4,16 +4,20 @@ public class FirstClass extends SecondClass {
 
     public static void main(String[] args) {
 
-        // Just Extends: call global(static) fields
-//        System.out.println(colorPublicStatic);
-//        System.out.println(colorPublicStatic);
-//        System.out.println(COLOR_PUBLIC_STATIC_FINAL);
+        // 1) Just Extends: call global(static) fields
+        System.out.println(colorPublicStatic);
+        System.out.println(colorPublicStatic);
+        System.out.println(COLOR_PUBLIC_STATIC_FINAL);
+        staticMethod();
 
-        // Just Object: call non-static fields(No global fields)
+        // 2) Just Object: call non-static fields(No global fields)
         SecondClass ob = new SecondClass();
         System.out.println(ob.colorNbr);
         System.out.println(ob.colorPublic);
         System.out.println(ob.colorString);
+        ob.nonStaticMethod();
+
+        // 3) Extends + Object = You can use all types of fields and method in the class
 
 
 
--- src/test/java/den/SecondClass.java
+++ src/test/java/den/SecondClass.java
@@ -2,17 +2,24 @@ package den;
 
 public class SecondClass {
 
+    // Non-Static fields:
     int colorNbr = 12;
     String colorString = "colorString";
     public String colorPublic = "colorPublic";
 
 
-    // Global:
+    // Static fields(variables) means Global:
     static String colorStatic = "colorStatic";
     public static String colorPublicStatic = "colorPublicStatic";
     public static final String COLOR_PUBLIC_STATIC_FINAL = "colorPublicStatic";
 
+    void nonStaticMethod(){
 
+    }
+
+    static void staticMethod(){
+
+    }
 
 
 }
